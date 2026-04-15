d020d860d9c78a1bf73ec7ec47cc9c2444d9f769
Tasbirul Alom <tasbirul07@gmail.com>
2026-04-15 18:37:33 +0600

    chore: increase deployment replicas to 2 and configure subnet availability zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,8 @@ resource "aws_subnet" "k8s_public_subnet" {
   cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
 
+  availability_zone = "${var.aws_region}a"
+
   tags = {
     Name = "k8s-public-subnet"
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy into"
   type        = string
-  default     = "us-east-1" # Update if your AMI is in a different region
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
